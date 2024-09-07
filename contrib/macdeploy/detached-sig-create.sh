6a81d8bdcedf6ae423fb485d3b3bc4b3e01da11f
MarkLTZ <team@litecoinz.info>
2024-09-07 14:46:38 +0000

    Rebrand: rename 'Bitcoin-Qt' to 'Zenium-Qt'

--- contrib/macdeploy/detached-sig-create.sh
+++ contrib/macdeploy/detached-sig-create.sh
@@ -7,8 +7,8 @@ export LC_ALL=C
 set -e
 
 ROOTDIR=dist
-BUNDLE="${ROOTDIR}/Bitcoin-Qt.app"
-BINARY="${BUNDLE}/Contents/MacOS/Bitcoin-Qt"
+BUNDLE="${ROOTDIR}/Zenium-Qt.app"
+BINARY="${BUNDLE}/Contents/MacOS/Zenium-Qt"
 SIGNAPPLE=signapple
 TEMPDIR=sign.temp
 ARCH=$(${SIGNAPPLE} info ${BINARY} | head -n 1 | cut -d " " -f 1)
--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -48,11 +48,11 @@ static const int TOOLTIP_WRAP_THRESHOLD = 80;
 
 #define QAPP_ORG_NAME "Bitcoin"
 #define QAPP_ORG_DOMAIN "bitcoin.org"
-#define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
-#define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
-#define QAPP_APP_NAME_TESTNET4 "Bitcoin-Qt-testnet4"
-#define QAPP_APP_NAME_SIGNET "Bitcoin-Qt-signet"
-#define QAPP_APP_NAME_REGTEST "Bitcoin-Qt-regtest"
+#define QAPP_APP_NAME_DEFAULT "Zenium-Qt"
+#define QAPP_APP_NAME_TESTNET "Zenium-Qt-testnet"
+#define QAPP_APP_NAME_TESTNET4 "Zenium-Qt-testnet4"
+#define QAPP_APP_NAME_SIGNET "Zenium-Qt-signet"
+#define QAPP_APP_NAME_REGTEST "Zenium-Qt-regtest"
 
 /* One gigabyte (GB) in bytes */
 static constexpr uint64_t GB_BYTES{1000000000};
